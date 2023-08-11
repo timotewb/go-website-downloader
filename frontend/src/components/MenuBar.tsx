643cd2e04a0694c05d63effde23b48ad2812a9fb
timotewb <timote.wb@gmail.com>
2023-08-11 19:16:02 +1200

    button stuff

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -2,42 +2,57 @@ import "./MenuBar.css";
 import downloadSVG from "../assets/images/download.svg";
 import gallerySVG from "../assets/images/gallery.svg";
 import activitySVG from "../assets/images/activity.svg";
-
-const setColorHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  e.currentTarget.classList.toggle("button");
-};
+import { useState } from "react";
 
 function MenuBar() {
+  const [downloadState, setDownloadState] = useState(true);
+  const [galleryState, setGalleryState] = useState(false);
+  const [activityState, setActivityState] = useState(false);
+
+  const downloadID = "downloadButton";
+  const galleryID = "galleryButton";
+  const activityID = "activityButton";
+
+  const handleClick = (id: string) => {
+    if (id === downloadID) {
+      setDownloadState(true);
+      setGalleryState(false);
+      setActivityState(false);
+    } else if (id === galleryID) {
+      setDownloadState(false);
+      setGalleryState(true);
+      setActivityState(false);
+    } else if (id === activityID) {
+      setDownloadState(false);
+      setGalleryState(false);
+      setActivityState(true);
+    } else {
+      setDownloadState(true);
+      setGalleryState(false);
+      setActivityState(false);
+    }
+  };
+
   return (
     <div id="menuBar">
       <div
         id="downloadButton"
-        className="button"
-        onClick={(e) => {
-          console.log("Download");
-          e.preventDefault();
-          setColorHandler(e);
-          // e.target.style.backgroundColor = "black";
-          console.log(e.target);
-        }}
+        className={downloadState ? "button button-active" : "button"}
+        onClick={() => handleClick("downloadButton")}
       >
         <img src={downloadSVG}></img> Download
       </div>
       <div
         id="galleryButton"
-        className="button"
-        onClick={() => {
-          console.log("Gallery");
-        }}
+        className={galleryState ? "button button-active" : "button"}
+        onClick={() => handleClick("galleryButton")}
       >
         <img src={gallerySVG}></img> Gallery
       </div>
       <div
         id="activityButton"
-        className="button"
-        onClick={() => {
-          console.log("Activity");
-        }}
+        className={activityState ? "button button-active" : "button"}
+        onClick={() => handleClick("activityButton")}
       >
         <img src={activitySVG}></img> Activity
       </div>
@@ -45,4 +60,3 @@ function MenuBar() {
   );
 }
 export default MenuBar;
-//https://stackoverflow.com/questions/38980051/reactjs-adding-active-class-to-button
